b16c4540c44944e66331322a8dd4282508fe45c1
BOOMER <127319193+golona73@users.noreply.github.com>
2023-04-05 12:23:59 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,30 @@
+class Main {
+  public static void main(String[] args) {
+    int wiersz=4;
+    int kolumna=6;
+    for (int i = 1; i <= wiersz; i++) {
+         
+    for (int j = 1; j <= kolumna; j++) {
+            if (i == 1 || i == wiersz || j == 1 || j == kolumna) {
+               System.out.print("*");
+            } else {
+               System.out.print("*");
+            }
+         }
+         System.out.println();
+      }
+   }
+}
+
+
+
+
+
+
+    
+
+
+
+
+
+
